995c795b859ec7c69f54b52190234a36bcc76d37
Daniel <daniel@debian.attlocal.net>
2018-12-02 03:37:24 -0500

    Third commit

--- entity.lua
+++ entity.lua
@@ -1,4 +1,4 @@
-entity = {x= 0,y=0,velocity={x=0,y=0},frames={},currentFrame = 0,currentTime = 0,health=100}
+entity = {x= 0,y=0,velocity={x=0,y=0,limit = 1},friction=1,frames={},currentFrame = 0,currentTime = 0,health=100}
 
 function entity:start()
     self.x = 0
@@ -8,6 +8,8 @@ function entity:start()
     self.health = 100
     self.currentFrame = 0
     self.currentTime = 0
+    self.velocity.limit = 1
+    self.friciton = 1
 end
 
 function entity:update(dt)
@@ -24,6 +26,43 @@ function entity:animate(dt,timer)
 	self.currentTime = 0
 	self.currentFrame = self.currentFrame + 1
     end
+end
+
+function entity:velocityMag() --velocity magnitude
+    return math.sqrt(self.velocity.x*self.velocity.x + self.velocity.y+self.velocity.y)
+end
+
+function entity:move(dx,dy,dt) --tried to add some physics and acceleration for smooth movement, still need to work out the kinks
+--[[    if dx ~= 0 or dy ~= 0 then
+	if self:velocityMag() < self.velocity.limit then
+	    self.velocity.x = self.velocity.x + dx*dt
+	    self.velocity.y = self.velocity.y + dy*dt
+	end
+    end
+    if dx == 0 then
+	if self.velocity.x > 0 then
+	    self.velocity.x = self.velocity.x - self.friction*dt
+	elseif self.velocity.x < 0 then
+	    self.velocity.x = self.velocity.y + self.friction*dt
+	end
+	if math.abs(self.velocity.x) < 0.05 then
+	    self.velocity.x = 0
+	end
+    end
+    if dy == 0 then
+	if self.velocity.y > 0 then
+	    self.velocity.y = self.velocity.y - self.friction*dt
+	elseif self.velocity.y < 0 then
+	    self.velocity.y = self.velocity.y + self.friction*dt
+	end
+	if math.abs(self.velocity.y) < 0.05 then
+	    self.velocity.y = 0
+	end
+    end]]--
+    self.velocity.x = dx
+    self.velocity.y = dy
+    self.x = self.x + self.velocity.x*dt
+    self.y = self.y + self.velocity.y*dt
 end
 
 function entity:new (o)
--- player.lua
+++ player.lua
@@ -3,8 +3,8 @@ entity = require (".entity")
 player = entity:new()
 
 function player:start()
-    self.x=0
-    self.y=0
+    self.x=love.graphics.getWidth()/2
+    self.y=love.graphics.getHeight()/2
     self.velocity={x=0,y=0}
     self.frames[1] = love.graphics.newImage("Frames/Player/player_idle.png")
     self.frames[2] = love.graphics.newImage("Frames/Player/player_walk1.png")
@@ -15,6 +15,14 @@ function player:start()
     self.health = 200
     self.currentFrame = self.frames.idle
     self.currentTime = 0
+    self.velocity.limit = 250 --250
+    self.friction = 40 --40
+
+    self.gun = {
+	love.graphics.newImage("Frames/Player/gun.png"),
+	love.graphics.newImage("Frames/Player/gunfire.png"),
+	rotation = 0
+    }
 end
 
 function player:shoot(x,y)
@@ -26,6 +34,7 @@ function player:melee(x,y)
 end
 
 function player:update(dt)
+    local dvx,dvy,modifier = 0,0,250*(love.graphics.getHeight()/1920) --delta velocity(x) and delta velocity(y) modifier (300,)
     if self.velocity.x ~= 0 or self.velocity.y ~= 0 then
 	player:animate(dt,0.07)
 	if self.currentFrame == 6 then
@@ -35,16 +44,35 @@ function player:update(dt)
 	self.currentFrame = 1
     end
     if love.keyboard.isDown("d") then
-	self.velocity.x = 1
+	dvx = dvx+2
     else
-	self.velocity.x = 0
+	dvx = dvx+1
     end
+    if love.keyboard.isDown("a") then
+	dvx = dvx-2
+    else
+	dvx = dvx-1
+    end
+    if love.keyboard.isDown("w") then
+	dvy = dvy-2
+    else
+	dvy = dvy-1
+    end
+    if love.keyboard.isDown("s") then
+	dvy = dvy+2
+    else
+	dvy = dvy+1
+    end
+    player:move(dvx*modifier,dvy*modifier,dt)
 end
 
 function player:draw()
-    local mW,mH,rH = love.graphics.getWidth()/2,love.graphics.getHeight()/2,love.graphics.getHeight()
-    love.graphics.draw(self.frames[self.currentFrame],mW,mH,0,8*(rH/1920),8*(rH/1920))
-    love.graphics.print(self.velocity.x,200,200)
+    local rH = love.graphics.getHeight()
+    local modifier = rH/1920
+    local eightMod = 8*modifier
+    love.graphics.draw(self.frames[self.currentFrame],self.x,self.y,0,eightMod,eightMod)
+    love.graphics.draw(self.gun[1],self.x+3*eightMod, self.y+eightMod+31*modifier, self.gun.rotation, eightMod,eightMod,3,1)
+    love.graphics.print(self.velocity.x,200,1200)
 end
 
 return player
